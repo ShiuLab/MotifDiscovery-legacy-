84dc80d57e7a5dd37df0219f8dd912af9f9d3256
Christina Azodi <azodichr@msu.edu>
2016-01-06 17:39:29 -0500

    Update TPOT_test.py

--- TPOT_test.py
+++ TPOT_test.py
@@ -19,8 +19,7 @@ df = pd.read_csv(DF, sep='\t',header=0, index_col=0)
 print(df.info())
 
  
-X_train, X_test, y_train, y_test = train_test_split(df.iloc[:,1:], df['class'],  
-                                                    train_size=0.75)  
+X_train, X_test, y_train, y_test = train_test_split(df.iloc[:,1:], df['class'], train_size=0.75)  
   
 tpot = TPOT(generations=5, verbosity=2)  
 tpot.fit(X_train, y_train)  
